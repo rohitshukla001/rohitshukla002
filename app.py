c25f4db568e0be2e3c864bb1aae6600a3766852f
Rohit shukla <91105211+rohitshukla001@users.noreply.github.com>
2021-11-04 21:09:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def predict():
         else:
             return render_template('index.html', prediction=[1])
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
